d701eaa3c831f8422384d98f1c64119bb1f285ba
Samuel Usoroh <usorohdev@gmail.com>
2024-04-17 08:19:29 +0100

    deploy app

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,8 +3,8 @@ import "./footer.scss"
 const Footer = () => {
   return (
     <div className="footer">
-      <span>lamadmin</span>
-      <span>© Lama Dev Admin Dashboard</span>
+      <span>Rechiex games</span>
+      <span>© Rechiex games Admin Dashboard</span>
     </div>
   )
 }
